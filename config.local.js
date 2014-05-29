0d11ea549a71ca50518fb9d3bf4fb695e3c9717e
Didier Dorelien <didier.dorelien@oomediahaiti.com>
2014-05-29 11:17:35 -0400

    Add config.local.js
    Add config.local.js

---
+++ config.local.js
@@ -0,0 +1,11 @@
+exports.host_url = "http://ooauth.herokuapp.com/";
+exports.port = process.env.PORT || 80;
+if (process.env.REDISTOGO_URL) {
+    var redis_conf = process.env.REDISTOGO_URL.match(/redis:\/\/(.+):(.+)@(.+):(.+)\//);
+    exports.redis = {
+        port: redis_conf[4],
+        host: redis_conf[3],
+        password: redis_conf[2],
+        database: redis_conf[1]
+    }
+}
